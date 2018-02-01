488ff7a9ccf75e8d643eeecdd01dcbe8c62a0b2a
Mohd Sheeraz <sheeraz@42race.com>
2018-02-01 16:53:10 +0800

    fixed runners

--- server/mobile-methods/virtual-races.js
+++ server/mobile-methods/virtual-races.js
@@ -21,7 +21,7 @@ Meteor.methods({
 			] 
 		}, { fields: {'orderNum': 1, 'status': 1, createdAt: 1 } });
 		let orders = Orders.find({'race': race, 'status': 'paid'}, {fields:{userID:1}});
-		let orderCount = orders.count();
+		let orderCount = AllResults.find({raceID: raceID}, {fields: {userID: 1}}).count();
 		let productItems = ProductItems.find({$or: [{race: race}, {race: 'all'}] }).fetch();
 		let teams = Teams.find({raceID: raceID}, {limit: 10, sort: {position: 1} } ).fetch();
 
@@ -109,7 +109,7 @@ Meteor.methods({
         return VirtualRaces.find({ end_date: {$lt: new Date()} }, options).fetch()
 	},
 	'virtualRaces.runnerForRace'(raceID){
-        let orders = _.uniq(Orders.find({raceID: raceID}, {
+        let orders = _.uniq(AllResults.find({raceID: raceID}, {
             fields: {userID: 1}
         }).fetch().map(function(x) {
             return x.userID;
@@ -133,7 +133,7 @@ Meteor.methods({
             sort: {createdAt: -1},
 			fields: fields
         };
-        let orders = _.uniq(Orders.find({raceID: raceID}, {
+        let orders = _.uniq(AllResults.find({raceID: raceID}, {
             fields: {userID: 1}
         }).fetch().map(function(x) {
             return x.userID;
